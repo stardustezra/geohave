188c5c107a99cbc1021b646840094239782c3a45
Sara Gylden <saragbusch@live.dk>
2024-03-27 09:49:12 +0100

    responsiv navbar

--- geo-have/src/components/Menu.vue
+++ geo-have/src/components/Menu.vue
@@ -10,10 +10,14 @@
       <div class="search-icon">
         <img :src="Search" alt="search" />
       </div>
-      <ul v-show="mobile" class="navigation">
+      <button class="burger-menu" @click="toggleMobileMenu">
+        <img :src="BurgerMenu" alt="menu" />
+      </button>
+      <ul :class="{ 'navigation': !mobile, 'mobile-menu': mobile }">
         <li>
           <router-link to="/" class="home">Home</router-link>
         </li>
+        <!-- Add more menu items as needed -->
       </ul>
     </nav>
   </header>
@@ -24,9 +28,19 @@ import { ref } from "vue";
 import Search from "../assets/icons/Search_White.png";
 import Ticket from "../assets/icons/Ticket.png";
 import Geoh from "../assets/icons/Geoh.png";
+import BurgerMenu from "../assets/icons/BurgerMenu.svg";
 
-const scrollPosition = ref(false); // Assuming this is your scrollPosition variable
-const mobile = ref(true); // Assuming this is your mobile variable
+const scrollPosition = ref(false);
+const mobile = ref(window.innerWidth < 768); 
+
+function toggleMobileMenu() {
+  mobile.value = !mobile.value;
+}
+
+// Update mobile value when window size changes
+window.addEventListener('resize', () => {
+  mobile.value = window.innerWidth < 768;
+});
 </script>
 
 <style scoped>
@@ -36,5 +50,24 @@ nav {
   justify-content: space-between;
   align-items: center;
   background-color: #2c5e36;
+}
+
+/* Styling for burger menu */
+.burger-menu {
+  display: none; 
+}
+
+@media (max-width: 767px) {
+  .burger-menu {
+    display: block; /* Show burger menu button for screens smaller than 768px */
+  }
+
+  .navigation {
+    display: none; /* Hide navigation links by default on smaller screens */
+  }
+
+  .mobile-menu {
+    display: block; /* Show mobile menu on smaller screens */
+  }
 }
 </style>
