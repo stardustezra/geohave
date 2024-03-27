2507b6ec657507282ec03979afbec73b27e1d08a
Ezra Andersen <stardustezra@gmail.com>
2024-03-27 12:27:41 +0100

    started implementing map

--- geo-have/src/views/HomeView.vue
+++ geo-have/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Menu from "../components/Menu.vue";
 import Cardcomponent from "@/components/Cardcomponent.vue";
+import Map from "@/components/Map.vue";
 </script>
 
 <template>
   <Menu />
   <Cardcomponent title="Skattejagt" />
+  <Map />
 </template>
