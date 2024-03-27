017aa4384ea01a465587845eb001b72765500922
Sara Gylden <saragbusch@live.dk>
2024-03-27 12:58:33 +0100

    navbar finish
    needs to add logout when clicked

--- geo-have/src/components/Menu.vue
+++ geo-have/src/components/Menu.vue
@@ -10,31 +10,37 @@
       <div class="search-icon">
         <img :src="Search" alt="search" />
       </div>
-      <button class="burger-menu" @click="toggleMobileMenu">
-        <img :src="BurgerMenu" alt="menu" />
-      </button>
+      <div :class="{ 'open': isOpen }" @click="toggleMenu" class="nav-icon" id="nav-icon3">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   </header>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
+
+const isOpen = ref(false);
+
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value;
+};
+
 import Search from "../assets/icons/Search_White.png";
-import Ticket from "../assets/icons/Ticket.png";
+import Ticket from "../assets/icons/billet.png";
 import Geoh from "../assets/icons/Geoh.png";
-import BurgerMenu from "../assets/icons/BurgerMenu.svg";
 
 const scrollPosition = ref(false);
 const mobile = ref(window.innerWidth < 768); 
-
-function toggleMobileMenu() {
-  mobile.value = !mobile.value;
-}
 
 // Update mobile value when window size changes
 window.addEventListener('resize', () => {
   mobile.value = window.innerWidth < 768;
 });
+
 </script>
 
 <style scoped>
@@ -44,15 +50,109 @@ nav {
   justify-content: space-between;
   align-items: center;
   background-color: #2c5e36;
+  padding: 10px; 
+  height: 70px; 
+}
+
+.nav-logo {
+  display: flex;
+  align-items: center;
+}
+
+/* Tilpas størrelsen af logoet baseret på skærmstørrelsen */
+.nav-logo img {
+  max-width: 100%;
+  max-height: 100%;
 }
 
 .ticket {
-  margin-bottom: -6px;
+  display: flex;
+  background-color: #FAE284;
+  width: 70px;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+}
+
+.ticket img, .search-icon img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.search-icon{
+  display: flex;
+  width: 70px;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+}
+
+.search-icon img {
+  width: 30px;
+  height: 30px;
+}
+
+.nav-icon {
+  width: 45px; 
+  height: 35px; 
+  position: relative;
+  margin: 0 20px; 
+  cursor: pointer;
+}
+
+.nav-icon span {
+  display: block;
+  position: absolute;
+  height: 7px; 
+  width: 100%;
+  background: #fff;
+  border-radius: 7px; 
+  opacity: 1;
+  left: 0;
+  transition: .25s ease-in-out;
+}
+
+/* Initial state of the spans */
+.nav-icon span:nth-child(1) {
+  top: 0px;
+}
+
+.nav-icon span:nth-child(2), .nav-icon span:nth-child(3) {
+  top: 14px; /* Adjust the top position of spans */
+}
+
+.nav-icon span:nth-child(4) {
+  top: 28px; /* Adjust the top position of spans */
+}
+
+/* When the menu is open, transform the spans */
+.nav-icon.open span:nth-child(1) {
+  top: 14px; /* Adjust the top position of spans */
+  width: 0%;
+  left: 50%;
+}
+
+.nav-icon.open span:nth-child(2) {
+  transform: rotate(45deg);
+}
+
+.nav-icon.open span:nth-child(3) {
+  transform: rotate(-45deg);
+}
+
+.nav-icon.open span:nth-child(4) {
+  top: 14px; /* Adjust the top position of spans */
+  width: 0%;
+  left: 50%;
 }
 
 /* Styling for burger menu */
 .burger-menu {
   display: none; 
+}
+
+.burger-menu .nav-icon {
+  display: block;
 }
 
 @media (max-width: 767px) {
@@ -66,6 +166,61 @@ nav {
 
   .mobile-menu {
     display: block; /* Show mobile menu on smaller screens */
+  }
+
+  .nav-icon span {
+    height: 4px; /* Juster tykkelsen efter behov */
+    border-radius: 4px; /* Juster hjørneradius efter behov */
+  }
+
+  .nav-logo img {
+    max-width: 190px; 
+    max-height: 43px; 
+    margin-right: 20px;
+  }
+
+  .ticket {
+    margin-right: 5px; /* Tilføj margen til højre for .ticket */
+  }
+
+  .ticket, .search-icon {
+    height: 45px;
+    width: 105px;
+  }
+
+  .ticket img, .search-icon img {
+    width: 50%; /* Mindre end før */
+    height: 50%; /* Mindre end før */
+  }
+  
+  .ticket {
+    width: 180px; /* Mindre end før */
+    height: 70px; /* Mindre end før */
+  }
+
+  .search-icon {
+  display: flex;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  margin-right: -10px; 
+}
+
+  .nav-icon span {
+    height: 2px; /* Adjust thickness as needed */
+    width: 25px; /* Adjust width as needed */
+  }
+
+  .nav-icon span:nth-child(1) {
+    top: 6px; /* Juster topafstanden for første span */
+  }
+
+  .nav-icon span:nth-child(2), .nav-icon span:nth-child(3) {
+    top: 16px; /* Juster topafstanden for andet og tredje span */
+  }
+
+  .nav-icon span:nth-child(4) {
+    top: 26px; /* Juster topafstanden for fjerde span */
   }
 }
 </style>
