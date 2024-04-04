218125acbb5c5c2799d644680edf33c6a21ce13c
Ezra Andersen <stardustezra@gmail.com>
2024-04-04 09:35:42 +0200

    delete files

--- geo-have/src/views/HomeView.vue
+++ geo-have/src/views/HomeView.vue
@@ -4,12 +4,10 @@ import SigninForm from "../components/SigninForm.vue";
 import SignupForm from "../components/SignupForm.vue";
 import SignOut from "../components/SignOut.vue";
 import Cardcomponent from "@/components/Cardcomponent.vue";
-import Map from "@/components/Map.vue";
 </script>
 
 <template>
   <Menu />
   <Cardcomponent title="Skattejagt" />
-  <Map />
 </template>
 ../components/SignupForm.vue../components/SigninForm.vue
