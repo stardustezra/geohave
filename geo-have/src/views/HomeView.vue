4ecb86698110c72603316cf8cbe625443dac22a6
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2024-04-05 12:37:15 +0200

    added signin/signup to navbar and auth state change for sign out

--- geo-have/src/views/HomeView.vue
+++ geo-have/src/views/HomeView.vue
@@ -1,14 +1,9 @@
 <script setup>
-import Menu from "../components/Menu.vue";
-// import Menu from "../components/Menu.vue";
-import SigninForm from "../components/SigninForm.vue";
-import SignupForm from "../components/SignupForm.vue";
-import SignOut from "../components/SignOut.vue";
+import Navbar from "../components/Navbar.vue";
 import Cardcomponent from "@/components/Cardcomponent.vue";
 </script>
 
 <template>
-  <Menu />
+  <Navbar />
   <Cardcomponent title="Skattejagt" />
 </template>
-../components/SignupForm.vue../components/SigninForm.vue
--- geo-have/src/views/SigninView.vue
+++ geo-have/src/views/SigninView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import SigninForm from "../components/SigninForm.vue";
 import SignOut from "../components/SignOut.vue";
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
+  <Navbar />
   <SigninForm />
   <SignOut />
 </template>
