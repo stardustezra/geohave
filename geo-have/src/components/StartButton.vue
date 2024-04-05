36ce3ee44a753a45a9738321cfd539b08b0a3dc2
Sara Gylden <saragbusch@live.dk>
2024-04-05 11:55:06 +0200

    Finished start page + startbutton
    StartButton needs to be linked to our map page

---
+++ geo-have/src/components/StartButton.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="start-button-container">
+    <div class="start-button">
+        <button>START JAGTEN</button>
+    </div>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+
+.start-button-container {
+  display: flex;
+  justify-content: center; 
+  align-items: center; 
+  height: 200px;
+}
+
+.start-button {
+  width: 201px;
+  height: 74px;
+  background-color: #87A669;
+  border: none;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.start-button button {
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: none;
+  color: white;
+  font-size: 20px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 700;
+  cursor: pointer;
+  outline: none;
+}
+</style>
--- geo-have/src/components/StartHunt.vue
+++ geo-have/src/components/StartHunt.vue
@@ -6,24 +6,39 @@
       <div class="bullet-points">
         <div class="bullet-points-container">
           <ul>
-            <li>Test dine evner med multiple choice-spørgsmål.</li>
-            <li>Udfordre din viden med trivia.</li>
-            <li>Løs puslespil for at afsløre skjulte skatte.</li>
-            <li>Genkend ikoniske billeder fra verdensregionerne.</li>
+            <li class="bullet-item">
+              <p>Test dine evner med multiple choice-spørgsmål.</p>
+              <img src="../assets/icons/clipboard-list.svg" alt="Clipboard-list" />
+            </li>
+            <li class="bullet-item">
+              <p>Udfordre din viden med trivia.</p>
+              <img src="../assets/icons/question.svg" alt="Questionmark" />
+            </li>
+            <li class="bullet-item">
+              <p>Løs puslespil for at afsløre skjulte skatte.</p>
+              <img src="../assets/icons/Puzzle_piece.svg" alt="Puzzle piece" />
+            </li>
+            <li class="bullet-item">
+              <p>Genkend ikoniske billeder fra verdensregionerne.</p>
+              <img src="../assets/icons/Globe.svg" alt="Globe" />
+            </li>
           </ul>
         </div>
       </div>
     </div>
+    <StartButton />
   </template>
   
   <script setup>
+  import StartButton from "@/components/StartButton.vue";
+
   </script>
   
   <style scoped>
     .startpage {
       text-align: center;
       margin: 0 auto;
-      margin-top: 20px;
+      margin-top: 50px;
     }
   
     .header-text {
@@ -47,35 +62,77 @@
     }
   
     .bullet-points {
-        background-color: #FFF;
-        padding: 20px;
-        border-radius: 4px;
-        margin-top: 10px;
-        width: 90%; 
-        max-width: 600px; 
-        margin-left: auto; 
-        margin-right: auto; 
-        box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.1);
+      background-color: #FFF;
+      padding: 25px;
+      border-radius: 4px;
+      margin-top: 10px;
+      width: 90%; 
+      max-width: 600px; 
+      margin-left: auto; 
+      margin-right: auto; 
+      box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.1);
     }
 
     .bullet-points-container {
-        display: flex;
-        align-items: flex-start;
-        justify-content: center;
+      display: grid;
+      grid-template-rows: repeat(4, 0fr);
+      grid-column-gap: 0px;
+      grid-row-gap: 0px;
     }
 
     .bullet-points ul {
-        padding-left: 0;
-        margin-bottom: 10px;
-        text-align: left; 
-    }
+    list-style-type: none; 
+    padding-left: 0;
+    margin-bottom: -8px;
+    text-align: left; 
+  }
 
-    .bullet-points ul li {
-        margin-bottom: 10px;
-        font-family: "Open Sans", sans-serif;
-        font-weight: 300;
-        font-size: 12px;
-    }
+  .bullet-points ul li {
+    margin-bottom: 10px;
+    font-family: "Open Sans", sans-serif;
+    font-weight: 300;
+    font-size: 12px;
+    position: relative; 
+  }
+
+  .bullet-points ul li::before {
+    content: "\2022"; 
+    color: #000; 
+    font-size: 12px; 
+    margin-right: 8px; 
+    position: absolute; 
+    left: -20px; 
+    top: 50%; 
+    transform: translateY(-50%); 
+    margin-left: 8px;
+  }
+
+  .bullet-item {
+    display: grid;
+    grid-template-columns: 1fr auto; 
+    align-items: center; 
+  }
+
+  .bullet-item p {
+    margin: 0; 
+    font-family: "Open Sans", sans-serif;
+    font-weight: 300;
+    font-size: 12px;
+    text-align: left;
+  }
+
+  .bullet-item img {
+    margin-left: auto; 
+    width: 20px;
+  }
+
+  .bullet-item img[src$="Globe.svg"]{
+    width: 26px; 
+  }
+
+  .bullet-item img[src$="Puzzle_piece.svg"] {
+    width: 28px; 
+  }
 
   </style>
   
