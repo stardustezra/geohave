6b452f1055da9983115f77e3dca9892f2f81c9f6
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2024-04-08 10:08:13 +0200

    fixed old component name

--- geo-have/src/views/QuizView.vue
+++ geo-have/src/views/QuizView.vue
@@ -1,14 +1,11 @@
 <template>
-    <Menu />
-    <QuizPage />
+  <Navbar />
+  <QuizPage />
 </template>
 
 <script setup>
-import Menu from "@/components/Menu.vue";
-import QuizPage from '../components/QuizPage.vue';
-
+import Navbar from "@/components/Navbar.vue";
+import QuizPage from "../components/QuizPage.vue";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- geo-have/src/views/TreasureHuntView.vue
+++ geo-have/src/views/TreasureHuntView.vue
@@ -1,12 +1,12 @@
 <template>
-  <Menu />
+  <Navbar />
   <CardComponent title="Skattejagt" />
   <StartHunt />
 </template>
 
 <script setup>
 import StartHunt from "@/components/StartHunt.vue";
-import Menu from "../components/Menu.vue";
+import Navbar from "../components/Navbar.vue";
 import CardComponent from "@/components/CardComponent.vue";
 </script>
 
