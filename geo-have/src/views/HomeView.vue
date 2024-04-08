54da3a29a46b0e6b0b2e0485195c9dc3e37b66c7
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2024-04-08 10:00:22 +0200

    updated homeview.vue

--- geo-have/src/views/HomeView.vue
+++ geo-have/src/views/HomeView.vue
@@ -1,15 +1,11 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
-import Cardcomponent from "@/components/Cardcomponent.vue";
-import Menu from "../components/Menu.vue";
-import CardComponent from "@/components/CardComponent.vue";
+import Cardcomponent from "@/components/CardComponent.vue";
 import LandingpageWelcome from "@/components/LandingpageWelcome.vue";
 </script>
 
 <template>
   <Navbar />
-  <Cardcomponent title="Skattejagt" />
-  <Menu />
   <CardComponent title="Skattejagt" />
   <LandingpageWelcome />
 </template>
