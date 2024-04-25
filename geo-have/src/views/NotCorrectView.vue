a8a193c4b988b459bfd5e30fb76f409700868a43
Sara Gylden <saragbusch@live.dk>
2024-04-25 12:40:38 +0200

    Fixed NotCorrect page

--- geo-have/src/views/NotCorrectView.vue
+++ geo-have/src/views/NotCorrectView.vue
@@ -1,9 +1,11 @@
 <template>
     <Navbar />
+    <NotCorrect />
 </template>
 
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import NotCorrect from '@/components/NotCorrect.vue'
 
 </script>
 
