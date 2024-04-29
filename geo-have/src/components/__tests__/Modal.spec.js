f288798d9dc842b709a8cb6b273eb7e060ee8f5a
Ezra Andersen <stardustezra@gmail.com>
2024-04-29 10:14:41 +0200

    added modal test

--- geo-have/src/components/__tests__/Modal.spec.js
+++ geo-have/src/components/__tests__/Modal.spec.js
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import Modal from "@/components/Modal.vue";
+//hej
 
 describe("Modal", () => {
   it("emits 'close' event when close button is clicked", async () => {
