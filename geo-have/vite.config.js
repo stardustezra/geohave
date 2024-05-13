3ebf94eec2d28c566fc5ab77ee22959ce0cdfce2
Ezra Andersen <stardustezra@gmail.com>
2024-05-13 15:06:30 +0200

    moved manifest til vite config - ready for pwa

--- geo-have/vite.config.js
+++ geo-have/vite.config.js
@@ -5,7 +5,41 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Geografisk Have",
+        description: "A treasure hunt app for the visitors to have fun with",
+        icons: [
+          {
+            src: "../src/assets/appicons/Icon-20x20.png",
+            type: "image/png",
+            sizes: "60x60",
+          },
+          {
+            src: "../src/assets/appicons/Icon-40x40.png",
+            type: "image/png",
+            sizes: "80x80",
+          },
+          {
+            src: "../src/assets/appicons/Icon-60x60.png",
+            type: "image/png",
+            sizes: "180x180",
+          },
+          {
+            src: "../src/assets/appicons/Icon-marketing-1024x1024.png",
+            type: "image/png",
+            sizes: "1024x1024",
+          },
+        ],
+        display: "standalone",
+        start_url: "/index.html",
+        background_color: "#fef9ea",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
