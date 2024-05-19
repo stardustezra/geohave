911023fbf090cc0e355fec07fc1ddcaf344e59f5
Ezra Andersen <stardustezra@gmail.com>
2024-05-19 16:46:54 +0200

    manifest works deployed

--- geo-have/vite.config.js
+++ geo-have/vite.config.js
@@ -15,28 +15,29 @@ export default defineConfig({
         "Icon-40x40.png",
         "Icon-60x60.png",
         "Icon-marketing-1024x1024.png",
+        "favicon.ico",
       ],
       manifest: {
         name: "Geografisk Have",
         description: "A treasure hunt app for the visitors to have fun with",
         icons: [
           {
-            src: "./src/assets/appicons/Icon-20x20.png",
+            src: "./public/appicons/Icon-20x20.png",
             type: "image/png",
             sizes: "60x60",
           },
           {
-            src: "./src/assets/appicons/Icon-40x40.png",
+            src: "./public/appicons/Icon-40x40.png",
             type: "image/png",
             sizes: "80x80",
           },
           {
-            src: "./src/assets/appicons/Icon-60x60.png",
+            src: "./public/appicons/Icon-60x60.png",
             type: "image/png",
             sizes: "180x180",
           },
           {
-            src: "./src/assets/appicons/Icon-marketing-1024x1024.png",
+            src: "./public/appicons/Icon-marketing-1024x1024.png",
             type: "image/png",
             sizes: "1024x1024",
           },
